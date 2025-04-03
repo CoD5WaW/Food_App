0b613320f079e2e67d26f0b7ff93f3035fefb2f4
Joshua Gabriel Soriano <joshuagabriel011@gmail.com>
2025-04-03 11:04:20 +0800

    added basic signup logic

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -10,10 +10,47 @@ export default function Signup() {
   const [phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSignup = () => {
-    // Implement signup logic here
-    // For now, just navigate to the main app
-    router.replace('/(tabs)');
+  const handleSignup = async () => {
+    // Signup logic 1
+    try {
+      // Put Loading animation logic here
+
+      // Prep data to send to api
+      const signupData = {
+        username: username, //idk if mugana nga same name sa variable sa taas
+        password: password
+      }
+
+      // API Call
+      const response = await fetch('http://capstone-testproject-backend-djangoapi.onrender.com/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(signupData)
+      });
+
+      // Parse response
+      const data = await response.json();
+
+      // Check response
+      if (response.status != 200) {
+        throw new Error(data.message || 'Signup failed'); // Throw error
+      }
+
+      // If successful then go to home
+      router.replace('/(tabs)');
+    }
+    catch (error) {
+      console.error('Signup error: ', error); // put error to console
+      throw new Error('Signup Failed', error.message) // alert user to error
+    }
+    finally {
+
+    }
+
+
+    
   };
 
   return (
